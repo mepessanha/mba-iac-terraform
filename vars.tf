e5e32f38ab094c1edc542446c5096b15fe3bc1f0
Maria Eduarda Pessanha <123198621+mepessanha@users.noreply.github.com>
2024-12-18 15:59:53 -0300

    Creating vars.tf

---
+++ vars.tf
@@ -0,0 +1,39 @@
+variable "region" {
+  description = "AWS region"
+  default     = "us-east-1"
+}
+
+variable "aws_access_key" {
+  description = "AWS Access Key"
+  type        = string
+  sensitive   = true
+}
+
+variable "aws_secret_key" {
+  description = "AWS Secret Key"
+  type        = string
+  sensitive   = true
+}
+
+variable "aws_session_token" {
+  description = "AWS Session Token"
+  type        = string
+  sensitive   = true
+}
+
+variable "db_user" {
+  description = "Usuário admini do banco de dados"
+  type        = string
+  default     = "db_admin"
+}
+
+variable "db_password" {
+  description = "Senha do usuário admin do banco de dados"
+  type        = string
+  sensitive   = true
+}
+
+variable "ec2_key_name" {
+  description = "Nome da chave SSH para acessar a instância EC2"
+  type        = string
+}
